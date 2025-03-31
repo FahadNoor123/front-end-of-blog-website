64d90310585ea80a8b5bf530fa82973858eaec96
Fahad <fahadnoor6677@gmail.com>
2025-03-31 21:35:12 +0500

    next config backend route added

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*',
+        destination: 'https://backend-of-blog-website.vercel.app/api/:path*',
       },
     ];
   },
