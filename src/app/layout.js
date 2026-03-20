d8fdecbdae530517a63f7a429487700991fa2d09
Fahad <fahad266233@gmail.com>
2026-03-20 14:56:44 +0500

    google analytics code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,26 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+       <head>
+        {/* Google Analytics */}
+        <Script 
+          async 
+          src={`https://www.googletagmanager.com/gtag/js?id=G-4HYRL328L7`}
+          strategy="afterInteractive"
+        />
+        <Script 
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4HYRL328L7');
+            `,
+          }}
+        />
+      </head>
       <body>
         {/* ✅ Use ClientLayout for normal users, AdminLayout will handle itself */}
         <MantineProvider withGlobalStyles withNormalizeCSS>
